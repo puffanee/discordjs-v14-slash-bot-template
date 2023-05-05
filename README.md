499f8d71ff0698d5c5349d17a904cdabb2630003
Ahmet Mert Kaymaz <mert@ahmetmertkaymaz.com.tr>
2023-05-05 23:09:14 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# BASIC EMPTY DİSCORD.JS V14 SLASH BOT TEMPLATE
+
+### This is a simple and empty (1 command) Discord.JS v14 template with slash command support.<br><br>
+
+# Config
+### Configuration can be edited in 'config.json'.<br>
+
+## Token
+### This is your bot's token at https://discord.com/developers/applications.<br><br>
+
+## Activity
+### Activity name, type and status can be edited in 'config.json'.
+- <b>activityName</b>:  Activity name to display in bot's status.
+- <b>activityType</b>:  The type of activity to display in the bot's status. (Competing, Listening, Playing, Streaming or Watching)
+- <b>activityStatus</b>:  Status of bot.<br>(dnd, online, idle, inivisble)
+
+<br>
+
+# Start
+### Type 'npm install' into the terminal to install packages.
+### Type 'node index.js' into the terminal to start the bot.
---
+++ index.js
@@ -0,0 +1,61 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const INTENTS = Object.values(GatewayIntentBits);
+const PARTIALS = Object.values(Partials);
+const client = new Client({
+    intents: [
+      INTENTS,
+    ],
+    allowedMentions: {
+        parse: ["users"]
+    },
+    partials: PARTIALS,
+    retryLimit: 3
+});
+
+global.client = client;
+client.commands = (global.commands = []);
+
+const { readdirSync } = require("fs")
+const { token } = require("./config.json");
+
+/* LOAD SLASH COMMANDS */
+
+readdirSync('./commands').forEach(f => {
+  if(!f.endsWith(".js")) return;
+
+ const props = require(`./commands/${f}`);
+
+ client.commands.push({
+       name: props.name.toLowerCase(),
+       description: props.description,
+       options: props.options,
+       dm_permission: props.dm_permission,
+       type: 1
+ });
+
+console.log(`[🟩 COMMAND] '${props.name}' loaded.`)
+
+});
+
+/* LOAD EVENTS */
+
+readdirSync('./events').forEach(e => {
+
+  const eve = require(`./events/${e}`);
+  const name = e.split(".")[0];
+
+  client.on(name, (...args) => {
+            eve(client, ...args)
+        });
+
+console.log(`[🟩 EVENT] '${name}' loaded.`)
+
+});
+
+/* LOGIN CLIENT */
+
+client.login(token).then(app => {
+  console.log(`[🟩 BOT] Token login successful.`)
+}).catch(app => {
+  console.log(`[🟥 BOT] Token login failed.`)
+})
